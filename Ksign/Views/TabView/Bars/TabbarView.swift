0116ecb7d9d8b9f40af111687511689cf2957f7c
3aiOS-dev <loveunless15@gmail.com>
2026-08-25 16:00:27 +0300

    Update TabbarView.swift

--- Ksign/Views/TabView/Bars/TabbarView.swift
+++ Ksign/Views/TabView/Bars/TabbarView.swift
@@ -1,24 +1,26 @@
 //
 //  TabbarView.swift
-//  feather
-//
-//  Created by samara on 23.03.2025.
+//  KINDA
 //
 
 import SwiftUI
 
 struct TabbarView: View {
-	@State private var selectedTab: TabEnum = .sources
 
-	var body: some View {
-		TabView(selection: $selectedTab) {
-			ForEach(TabEnum.defaultTabs, id: \.hashValue) { tab in
-				TabEnum.view(for: tab)
-					.tabItem {
-						Label(tab.title, systemImage: tab.icon)
-					}
-					.tag(tab)
-			}
-		}
-	}
+    @State private var selectedTab: TabEnum = .home
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            ForEach(TabEnum.defaultTabs, id: \.self) { tab in
+                TabEnum.view(for: tab)
+                    .tabItem {
+                        Label(
+                            tab.title,
+                            systemImage: tab.icon
+                        )
+                    }
+                    .tag(tab)
+            }
+        }
+    }
 }
