452f903d2c8372784ab7cb3b9f9ca8e39ae985d2
3aiOS-dev <loveunless15@gmail.com>
2026-08-26 12:25:07 +0300

    Update ExtendedTabbarView.swift

--- Ksign/Views/TabView/Bars/ExtendedTabbarView.swift
+++ Ksign/Views/TabView/Bars/ExtendedTabbarView.swift
@@ -1,29 +1,35 @@
 //
-//  ExtendedTabbarView.swift
-//  KINDA
+// ExtendedTabbarView.swift
+// KINDA
 //
 
 import SwiftUI
 
 @available(iOS 18, *)
 struct ExtendedTabbarView: View {
-
     @AppStorage("Feather.tabCustomization")
     private var customization = TabViewCustomization()
 
     var body: some View {
         TabView {
-            ForEach(TabEnum.defaultTabs, id: \.self) { tab in
+            ForEach(
+                TabEnum.defaultTabs,
+                id: \.self
+            ) { tab in
                 Tab(
                     tab.title,
                     systemImage: tab.icon
                 ) {
                     TabEnum.view(for: tab)
                 }
-                .customizationID("tab.\(tab.rawValue)")
+                .customizationID(
+                    "tab.\(tab.rawValue)"
+                )
             }
         }
         .tabViewStyle(.sidebarAdaptable)
-        .tabViewCustomization($customization)
+        .tabViewCustomization(
+            $customization
+        )
     }
 }
